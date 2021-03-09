4e0135fed1c6a35b03eaabf385028a08680b53c8
ha-nagisa <nagisa18222@icloud.com>
2021-03-09 23:46:57 +0900

    server you can find location

--- front/src/index.jsx
+++ front/src/index.jsx
@@ -1,6 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = () => <di>hello react!!</di>;
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+
+
+import dayjs from "dayjs";
+import "dayjs/locale/ja";
+import DayjsUtils from "@date-io/dayjs";
+import { MuiPickersUtilsProvider } from "@material-ui/pickers";
+
+
+import CalendarBoard from  "./components/CalendarBoard/container";
+import Navigation from "./components/Navigation/container";
+import rootReducer from "./redux/rootReducer";
+import AddScheduleDialog from "./components/AddScheduleDialog/container";
+import CurrentScheduleDialog from "./components/CurrentScheduleDialog/container";
+import ErrorSnackbar from "./components/ErrorSnackbar/contaienr"
+
+dayjs.locale("ja");
+
+const store = createStore(rootReducer,applyMiddleware(thunk));
+
+const App = () => (
+  <Provider store={store}>
+    <MuiPickersUtilsProvider utils={DayjsUtils}>
+      <Navigation />
+      <CalendarBoard />
+      <AddScheduleDialog/>
+      <CurrentScheduleDialog />
+      <ErrorSnackbar />
+    </MuiPickersUtilsProvider>
+  </Provider>
+  );
 
 ReactDOM.render(<App />, document.getElementById("root"));
